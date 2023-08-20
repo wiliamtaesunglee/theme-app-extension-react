3c0c8055340b9b76a87ce5e13e6b231fc2b13740
wiliamtaesunglee <wiliam.tae@gmail.com>
2023-08-20 19:08:37 -0300

    generate xml

--- web/routes/app_proxy/index.js
+++ web/routes/app_proxy/index.js
@@ -1,15 +1,67 @@
 import { Router } from 'express';
-import shopify from '../../shopify.js';
-import axios from 'axios';
+import { create } from 'xmlbuilder2';
+
+
+function extractShopName(url) {
+  const match = url.match(/(?:https:\/\/)?(.*?)\.myshopify\.com/);
+  return match ? match[1] : null;
+}
+
+function generateGoogleShoppingXML(products, shop) {
+  const shopName = extractShopName(shop);
+  console.log('*****', extractShopName(shop), '*****', shop)
+
+  const xml = create({ version: '1.0', encoding: 'UTF-8' })
+    .ele('rss', { 'xmlns:g': 'http://base.google.com/ns/1.0', version: '2.0' })
+      .ele('channel')
+        .ele('title', shopName)
+        .up()
+        .ele('link', shop)
+        .up()
+        .ele('description').txt(`${shopName} products`)
+        .up();
+
+
+  products.forEach(product => {
+    xml.ele('item')
+      .ele('g:id', product.id)
+      .up()
+      .ele('g:title').txt(product.title)
+      .up()
+      .ele('g:description').txt(product.description)
+      .up()
+      .ele('g:link').txt(product.link)
+      .up()
+      .ele('g:image_link').txt(product.imageLink)
+      .up()
+      .ele('g:price').txt(product.price)
+      .up()
+      .ele('g:availability').txt(product.availability)
+      .up()
+      .ele('g:brand').txt(product.brand)
+      .up();
+  });
+
+  return xml.end({ prettyPrint: true });
+}
 
 export const proxyRouter = Router();
 
 proxyRouter.get('/products', async (_req, res) => {
-  // try {
-  //   const data = axios.get('https://google-shopping-pix-feature.myshopify.com/admin/api/2021-07/products.json')
-  //   // console.log('data ', data)
-  // } catch (e) {
-  //   console.log('error')
-  // }
-  return res.status(200).send({ content: res.locals.products });
+  const products = res.locals.products.data.map((product) => {
+    return {
+      id: product.id,
+      title: product.title,
+      description: product.body_html,
+      link: `https://${res.locals.user_shop}/products/${product.title?.toLowerCase().replace(/ /g, '-')}`,
+      imageLink: product.image?.src,
+      price: product.variants[0].price * 0.95,
+      availability: product.variants[0].inventory_quantity > 0 ? 'in stock' : 'out of stock',
+      brand: product.vendor,
+    };
+  });
+
+  const xml = generateGoogleShoppingXML(products, res.locals.user_shop);
+
+  return res.set('Content-Type', 'text/xml').status(200).send(xml);
 }); 
